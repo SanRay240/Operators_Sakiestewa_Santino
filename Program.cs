29b9097ad37dbd195bb3c4e27af5e231429d356f
SanRay240 <real.sanray240@gmail.com>
2025-03-07 15:15:30 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Operators_Sakiestewa_Santino
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int intmlt = 6 * 45;
+
+            int intAdd = 55 + 66;
+
+            Console.WriteLine($"The max of {intMult} and {intAdd} is {Math.Max(intMult, intAdd)}"). " +
+                $"The min of the two is {Math.Min(intMult, intAdd)}.");
+        }
+    }
+}
